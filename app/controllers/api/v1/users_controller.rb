1e7cc1177c406ad793e8117ea07400f022fc601b
Vourliotis <x.vourliotis@gmail.com>
2021-09-02 17:29:12 +0300

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :find_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   def index
     render json: User.all
@@ -39,5 +40,9 @@ class Api::V1::UsersController < ApplicationController
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
   end
 end
